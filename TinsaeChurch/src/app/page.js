0a9bee0bd180201f84fd674424d162af03e9e0e2
jt-qwerty <janet.tsegazeab@gmail.com>
2024-05-28 12:53:47 -0600

    added smooth scrolling to the main page

--- TinsaeChurch/src/app/page.js
+++ TinsaeChurch/src/app/page.js
@@ -5,13 +5,23 @@
 import Cards from './cards/page.js'
 import BgVid from "./components/BgVid"
 import ContactCard from "./components/ContactCard"
-
+import React, {useEffect } from 'react';
+import Lenis from 'lenis';
 
 export default function Home() {
 
-  // useEffect(() => {
-  //   const lenis = new 
-  // })
+  useEffect(() => {
+    const lenis = new Lenis() 
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf) 
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
+
+
   return (
       <div>
         <BgVid/>
