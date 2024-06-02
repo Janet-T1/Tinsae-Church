b30ee428825daaca7eedf41767db41ad5fd9a8a9
jt-qwerty <janet.tsegazeab@gmail.com>
2024-06-02 00:06:32 -0600

    finished the get involved form

--- TinsaeChurch/src/app/components/GetInvolvedForm.js
+++ TinsaeChurch/src/app/components/GetInvolvedForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const GetInvolvedForm = () => {
     return (
-        <div className="w-screen h-screen flex items-center justify-center bg-[#2b2b2b]">
-            <div className="w-full md:w-3/4 lg:w-2/3 bg-[#121211] p-12 mt-10">
-                <h1 className="text-3xl font-bold mb-6 text-center text-white">Join Our Family</h1>
+        <div className="w-screen  flex items-center justify-center bg-[#2b2b2b] ">
+            <div className="md:w-3/4 lg:w-2/3 bg-[#121211] p-12 mt-24 mb-12">
+                <h1 className="text-5xl font-bold mb-6  text-white">Find Where You Belong</h1>
                 <form className="flex flex-col gap-4">
                     <div className="flex flex-col md:flex-row gap-4">
                         <input
@@ -19,25 +19,31 @@ const GetInvolvedForm = () => {
                         />
                     </div>
                     <input
-                        type="email"
-                        placeholder="Email"
-                        className="p-2 border bg-gray-100 text-black input-placeholder"
-                    />
-                    <input
-                        type="date"
-                        placeholder="Birthday"
-                        className="p-2 border bg-gray-100 text-black input-placeholder"
-                    />
-                    <input
                         type="tel"
                         placeholder="Phone Number"
                         className="p-2 border bg-gray-100 text-black input-placeholder"
                     />
+
+                    <h1 className=" font-bold mb-1 mt-3 text-white text-3xl font-bold">Pick the Ministry You'd like to Join</h1>
+                    <div className="flex flex-col gap-2 text-xl font-semibold ">
+                        {['Usher', 'Choir', 'Prayer', 'Evangelism', 'Social Media', 'Teaching'].map(ministry => (
+                            <label key={ministry} className="text-white flex items-center">
+                                <input
+                                    type="checkbox"
+                                    className="mr-2 h-6 w-6 accent-[#2b2b2b]"
+                                    name="ministry"
+                                    value={ministry.toLowerCase()}
+                                />
+                                {ministry}
+                            </label>
+                        ))}
+                    </div>
+
                     <button
                         type="submit"
-                        className="p-2 bg-red-700 text-white font-semibold rounded hover:bg-red-800 transition-colors text-xl"
+                        className="p-2 mt-4 bg-red-700 text-white font-semibold rounded hover:bg-red-800 transition-colors text-xl"
                     >
-                        Join
+                        Get Involved
                     </button>
                 </form>
             </div>
@@ -46,3 +52,4 @@ const GetInvolvedForm = () => {
 }
 
 export default GetInvolvedForm;
+
