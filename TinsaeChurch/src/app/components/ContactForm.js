7aa3a0232b7ca0f51ac8b27c4a24abced4939bd5
jt-qwerty <janet.tsegazeab@gmail.com>
2024-06-01 23:05:03 -0600

    final touches for contact form

--- TinsaeChurch/src/app/components/ContactForm.js
+++ TinsaeChurch/src/app/components/ContactForm.js
@@ -3,34 +3,34 @@ import React from 'react';
 const ContactForm = () => {
     return (
         <div className="w-screen h-screen flex items-center justify-center bg-[#2b2b2b]">
-            <div className="w-full md:w-3/4 lg:w-2/3 bg-[#1c1c1c] p-10 mt-10">
+            <div className="w-full md:w-3/4 lg:w-2/3 bg-[#121211] p-10 mt-10">
                 <h1 className="text-3xl font-bold mb-6 text-center text-white">Contact Us</h1>
                 <form className="flex flex-col gap-4">
                     <div className="flex flex-col md:flex-row gap-4">
                         <input
                             type="text"
                             placeholder="First Name"
-                            className="p-2 border bg-gray-200 text-black flex-1 input-placeholder"
+                            className="p-2 border bg-gray-100 text-black flex-1 input-placeholder"
                         />
                         <input
                             type="text"
                             placeholder="Last Name"
-                            className="p-2 border bg-gray-200 text-black flex-1 input-placeholder"
+                            className="p-2 border bg-gray-100 text-black flex-1 input-placeholder"
                         />
                     </div>
                     <input
                         type="email"
                         placeholder="Email"
-                        className="p-2 border bg-gray-200 text-black input-placeholder"
+                        className="p-2 border bg-gray-100 text-black input-placeholder"
                     />
                     <input
                         type="text"
                         placeholder="Subject"
-                        className="p-2 border bg-gray-200 text-black input-placeholder"
+                        className="p-2 border bg-gray-100 text-black input-placeholder"
                     />
                     <textarea
                         placeholder="Message"
-                        className="p-2 border bg-gray-200 text-black h-32 resize-none input-placeholder"
+                        className="p-2 border bg-gray-100 text-black h-32 resize-none input-placeholder"
                     ></textarea>
                     <button
                         type="submit"
