736fa863171c5c2ddc66326d635b234a7f1eb1ce
JanetTsegazeab <janet.tsegazeab@ucalgary.ca>
2025-03-10 09:08:25 -0600

    slide pictures now render correclty

--- TinsaeChurch/src/app/components/SliderData.js
+++ TinsaeChurch/src/app/components/SliderData.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 export const SliderData = [
     {
-        image: '/evanImgs/evan8.jpg',
+        image: '/evanImgs/evan8.JPG',
     },
     {
-        image: '/evanImgs/evan2.jpg',
+        image: '/evanImgs/evan2.JPG',
     },
     {
-        image: '/evanImgs/evan12.jpg',
+        image: '/evanImgs/evan12.JPG',
     },
     {
-        image: '/evanImgs/evan3.jpg',
-    },
-    
-    {
-        image: '/evanImgs/evan7.jpg',
+        image: '/evanImgs/evan3.JPG',
     },
     
     {
-        image: '/evanImgs/evan9.jpg',
+        image: '/evanImgs/evan7.JPG',
+    },
+    
+    {
+        image: '/evanImgs/evan9.JPG',
     },
     {
-        image: '/evanImgs/evan10.jpg',
+        image: '/evanImgs/evan10.JPG',
     },
     {
-        image: '/evanImgs/evan11.jpg',
+        image: '/evanImgs/evan11.JPG',
     },
    
 ]
